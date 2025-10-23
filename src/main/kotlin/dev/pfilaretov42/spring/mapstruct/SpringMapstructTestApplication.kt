9ce9e8a899b1ea30135b35f6bb1adfe01216c0fe
pfilaretov42 <petr.filaretov@gmail.com>
2025-10-23 10:11:04 -0700

    init commit

---
+++ src/main/kotlin/dev/pfilaretov42/spring/mapstruct/SpringMapstructTestApplication.kt
@@ -0,0 +1,11 @@
+package dev.pfilaretov42.spring.mapstruct
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMapstructTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringMapstructTestApplication>(*args)
+}
---
+++ src/test/kotlin/dev/pfilaretov42/spring/mapstruct/SpringMapstructTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.pfilaretov42.spring.mapstruct
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMapstructTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
