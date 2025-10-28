f9aca8bb752be0785acf2112a8b676301b97772b
pfilaretov42 <petr.filaretov@gmail.com>
2025-10-28 14:14:42 -0700

    apply fix 2: qualify method by name

--- src/main/kotlin/dev/pfilaretov42/spring/mapstruct/mapper/BalrogMapper.kt
+++ src/main/kotlin/dev/pfilaretov42/spring/mapstruct/mapper/BalrogMapper.kt
@@ -4,6 +4,7 @@ import dev.pfilaretov42.spring.mapstruct.dto.BalrogDto
 import dev.pfilaretov42.spring.mapstruct.model.Balrog
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
+import org.mapstruct.Named
 
 /**
  * Like a Balrog lurking in the depths of Moria, a seemingly harmless protected String->String
@@ -14,11 +15,10 @@ import org.mapstruct.Mapping
 @Mapper(componentModel = "spring")
 abstract class BalrogMapper {
 
-    @Mapping(target = "trueName", expression = "java(uppercased(dto.getTrueName()))")
+    @Mapping(target = "trueName", qualifiedByName = ["uppercased"])
     abstract fun toModel(dto: BalrogDto): Balrog
 
-    // This is the tricky method: it looks generic (String -> String), so MapStruct can pick it up
-    // and use it for ANY String mapping, not only where we want it.
+    @Named("uppercased")
     protected fun uppercased(value: String): String = value.uppercase()
 
     abstract fun toDto(model: Balrog): BalrogDto
